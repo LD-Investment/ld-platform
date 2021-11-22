ba448502b805948db11e8a21f7779b00a4cbbd6f
JinJis <david.jeong0611@gmail.com>
2021-11-23 02:11:12 +0900

    Remove static/html files and setup initial API url and router

--- config/api_router.py
+++ config/api_router.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from ld_platform.users.api.views import UserViewSet
@@ -13,3 +14,11 @@ router.register("users", UserViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
+
+# Your custom urls for API go here
+urlpatterns += [
+    # User management
+    # TODO: below is just example.
+    path("users/", include("ld_platform.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
+]
