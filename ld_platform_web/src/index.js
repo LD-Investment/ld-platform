82b93fe9dbc605956dd82644a987b4b55d639a6c
JinJis <david.jeong0611@gmail.com>
2021-12-18 22:22:43 +0900

    Integrate react template into ld-platform-web

--- ld_platform_web/src/index.js
+++ ld_platform_web/src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// core styles
+import "./scss/volt.scss";
+
+// vendor styles
+import "@fortawesome/fontawesome-free/css/all.css";
+import "react-datetime/css/react-datetime.css";
+
+import HomePage from "./pages/HomePage";
+import ScrollToTop from "./components/ScrollToTop";
+
+ReactDOM.render(
+  <HashRouter>
+    <ScrollToTop />
+    <HomePage />
+  </HashRouter>,
+  document.getElementById("root")
+);
