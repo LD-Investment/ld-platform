f6d1d09e374f34ea1ff4d1733dbc74d99f56ec18
JinJis <david.jeong0724@gmail.com>
2022-02-07 03:31:48 +0900

    Django Celery Beat setting for Coinness periodic scraper

---
+++ backend/ld_platform/apps/bots/managers.py
@@ -0,0 +1,55 @@
+import re
+from datetime import datetime
+from typing import List
+
+import pytz
+from django.db import models
+from django.utils import timezone
+
+
+class CoinnessNewsDataManager(models.Manager):
+    def create(self, *args, **kwargs):
+        """
+        Should parse raw data scraped from web, validate and persist
+        """
+        date = kwargs.pop("date", None)  # e.g 23:002022년 2월 5일 토요일
+        bull_bear = kwargs.pop(
+            "bull_bear", "Bull 0Bear 0"
+        )  # e.g Bull 268Bear 22\n공유하기업계동향
+
+        kwargs["date"] = self._parse_date(date) if date else timezone.now()
+        bull_bear = self._parse_bull_bear(bull_bear)
+        kwargs["bull_count"] = bull_bear[0]
+        kwargs["bear_count"] = bull_bear[1]
+
+        return super().create(**kwargs)
+
+    @staticmethod
+    def _parse_date(data: str) -> datetime:
+        """
+        Args:
+            data: "23:002022년 2월 5일 토요일"
+
+        Return:
+            <datetime.datetime object>
+        """
+        UTC = pytz.timezone("UTC")
+        KST = pytz.timezone("Asia/Seoul")
+
+        date_in_kr = data[5:-4]  # now converted to 2022년 2월 5일
+        obj = datetime.strptime(date_in_kr, "%Y년 %m월 %d일")
+        now_utc = obj.replace(tzinfo=UTC)
+        now_kst = now_utc.astimezone(KST)
+        return now_kst
+
+    @staticmethod
+    def _parse_bull_bear(data: str) -> List[int]:
+        """
+        Args:
+            data: "Bull 268Bear 22\n공유하기업계동향"
+
+        Return:
+            [0, 0]
+        """
+        result = re.findall(r"\d+", data)
+        return [int(s) for s in result]
---
+++ backend/ld_platform/apps/bots/tasks.py
@@ -0,0 +1,101 @@
+import logging
+import time
+
+from celery import shared_task
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from ld_platform.apps.bots.models import CoinnessNewsData
+
+logger = logging.getLogger(__name__)
+
+
+def _set_chrome_options() -> Options:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs: dict = {"profile.default_content_settings": {"images": 2}}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    return chrome_options
+
+
+@shared_task
+def scrap_coinness_news():
+    """Scraps Coinness News periodically
+
+    E.g https://coinness.live/news/1002000
+    에이프로빗 실시간 주요 암호화폐 시세            # title_selector
+    14:002021년 7월 22일 목요일               # date_selector
+    *BTC 37,731,000원 ▲4.60%               # content_selector
+    *ETH 2,315,000원 ▲6.83%
+    ...
+    Bull 14 Bear 11                       # bull_bear_selector
+    """
+
+    logger.info("Starting Coinness Web Scraper")
+    driver = webdriver.Remote(
+        command_executor="http://selenium-chrome:4444/wd/hub",
+        desired_capabilities=DesiredCapabilities.CHROME,
+        options=_set_chrome_options(),
+    )
+    driver.implicitly_wait(5)
+
+    COINNESS_BASE_URL = "https://coinness.live/news"
+
+    title_selector = "#root > div > div > main > div > div.sc-bBXrwG.gIQUtB > h3"
+    date_selector = (
+        "#root > div > div > main > div > div.sc-bBXrwG.gIQUtB > div.sc-iwyWTf.bIbYqN"
+    )
+    content_selector = "#root > div > div > main > div > div.sc-bBXrwG.gIQUtB > div.sc-lmoMya.jQLGnc > span"
+    bull_bear_selector = (
+        "#root > div > div > main > div > div.sc-bBXrwG.gIQUtB > div.sc-crrszt.fqZBup"
+    )
+
+    # Get the latest article number
+    ARTICLE_NUM = 1011000
+    qs = CoinnessNewsData.objects.all().order_by("-article_num")
+    if qs.exists():
+        ARTICLE_NUM = int(qs[0].article_num) + 1
+
+    logger.info(f"The latest article number is: {ARTICLE_NUM}")
+
+    MAX_RETRY = 10
+    CUR_TRIAL = 0
+    while CUR_TRIAL < MAX_RETRY:
+        try:
+            driver.get(url=f"{COINNESS_BASE_URL}/{ARTICLE_NUM}")
+            title = driver.find_element(By.CSS_SELECTOR, title_selector)
+            date = driver.find_element(By.CSS_SELECTOR, date_selector)
+            content = driver.find_element(By.CSS_SELECTOR, content_selector)
+            bull_bear = driver.find_element(By.CSS_SELECTOR, bull_bear_selector)
+
+            kwargs = {
+                "article_num": ARTICLE_NUM,
+                "title": title.text,
+                "date": date.text,
+                "content": content.text,
+                "bull_bear": bull_bear.text,
+            }
+
+            logger.info(f"Data parsed from HTML: {kwargs}")
+            CoinnessNewsData.objects.create(**kwargs)
+            CUR_TRIAL = 0
+
+        except NoSuchElementException as e:
+            logger.error(f"Coinness web scraper encountered error: {e.msg}")
+            CUR_TRIAL += 1
+            logger.info(f"Current Trial: {CUR_TRIAL}")
+
+        ARTICLE_NUM += 1
+        driver.close()
+        time.sleep(0.3)
+
+    logger.info("Coinness Web scraper finished. Stopping..")
+    driver.quit()
