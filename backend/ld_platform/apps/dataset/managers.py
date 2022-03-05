4235edd3c0ce080c5f100dd429f3f0533c13b4f8
JinJis <david.jeong0724@gmail.com>
2022-03-06 00:08:26 +0900

    Add datetime parser to Coinness scrpaer and change to XPATH selector

--- backend/ld_platform/apps/dataset/managers.py
+++ backend/ld_platform/apps/dataset/managers.py
@@ -13,15 +13,7 @@ class CoinnessNewsDataManager(models.Manager):
         Should parse raw data scraped from web, validate and persist
         """
         date = kwargs.pop("date", None)  # e.g 23:002022년 2월 5일 토요일
-        # bull_bear = kwargs.pop(
-        #     "bull_bear", "Bull 0Bear 0"
-        # )  # e.g Bull 268Bear 22\n공유하기업계동향
-
         kwargs["date"] = self._parse_date(date) if date else timezone.now()
-        # bull_bear = self._parse_bull_bear(bull_bear)
-        # kwargs["bull_count"] = bull_bear[0]
-        # kwargs["bear_count"] = bull_bear[1]
-
         return super().create(**kwargs)
 
     @staticmethod
@@ -36,8 +28,12 @@ class CoinnessNewsDataManager(models.Manager):
         UTC = pytz.timezone("UTC")
         KST = pytz.timezone("Asia/Seoul")
 
-        date_in_kr = data[5:-4]  # now converted to 2022년 2월 5일
-        obj = datetime.strptime(date_in_kr, "%Y년 %m월 %d일")
+        date_in_kr = data[5:-4]
+        time_in_kr = data[0:5]
+        full_date_in_kr = (
+            date_in_kr + " " + time_in_kr
+        )  # now converted to 2022년 2월 5일 23:00
+        obj = datetime.strptime(full_date_in_kr, "%Y년 %m월 %d일 %H:%M")
         now_utc = obj.replace(tzinfo=UTC)
         now_kst = now_utc.astimezone(KST)
         return now_kst
