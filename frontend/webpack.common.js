b8cc65a90dee7d6f2865c8836e1656915421228f
JinJis <david.jeong0724@gmail.com>
2022-03-20 22:47:08 +0900

    Fix node-sass compatibility issue for M1 silicon chip

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -43,7 +43,12 @@ module.exports = {
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
-          "sass-loader"
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            },
+          },
         ]
       }
     ]
