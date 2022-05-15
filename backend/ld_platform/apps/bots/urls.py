ed3c2d0b90479616c2aee975f43384cd9163504a
JinJis <david.jeong0724@gmail.com>
2022-05-16 01:52:23 +0900

    WIP: CryptoBERT backend2

--- backend/ld_platform/apps/bots/urls.py
+++ backend/ld_platform/apps/bots/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 app_name = "bots"
 
 urlpatterns = [
-    # Indicator Bot - General
+    # - Indicator Bot - General
     path("indicator"),
-    # Indicator Bot - News Tracker
+    # - Indicator Bot - News Tracker
     path("indicator/news-tracker"),
     path("indicator/news-tracker/subscribe"),
     path("indicator/news-tracker/control/fetch-news"),
     path("indicator/news-tracker/control/model"),
-    path("indicator/news-tracker/control/model/<int:id>"),
-    path("indicator/news-tracker/control/model/<int:id>/calculate"),
+    path("indicator/news-tracker/control/model/<int:model_id>"),
+    path("indicator/news-tracker/control/model/<int:model_id>/score"),
 ]
