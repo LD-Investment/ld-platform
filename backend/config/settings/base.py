77d68d8d9c814102da9c2a477166a2003d62ae40
JinJis <david.jeong0724@gmail.com>
2022-05-21 17:37:37 +0900

    Apply lint

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -245,7 +245,7 @@ LOGGING = {
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
-                      "%(process)d %(thread)d %(message)s"
+            "%(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
--- frontend/ld_platform_web/src/pages/auth/LogIn.js
+++ frontend/ld_platform_web/src/pages/auth/LogIn.js
@@ -1,150 +1,163 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleLeft, faIdCard, faUnlockAlt} from "@fortawesome/free-solid-svg-icons";
-import {Button, Card, Col, Container, Form, FormCheck, InputGroup, Row} from "@themesberg/react-bootstrap";
-import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faIdCard,
+  faUnlockAlt
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  FormCheck,
+  InputGroup,
+  Row
+} from "@themesberg/react-bootstrap";
+import { Link } from "react-router-dom";
 
-import {Routes} from "../../routes";
+import { Routes } from "../../routes";
 import BgImage from "../../assets/img/illustrations/signin.svg";
 import LdAxios from "ld-axios/axios";
 import useUserStore from "../../store/user_store_context";
 
 export default () => {
-    const [loginFailed, setLoginFailed] = React.useState(false);
-    const userStore = useUserStore();
+  const [loginFailed, setLoginFailed] = React.useState(false);
+  const userStore = useUserStore();
 
-    const onSubmitLoginForm = e => {
-        e.preventDefault();
-        LdAxios.post("/api/auth/login/", {
-            username: e.target[0].value,
-            email: e.target[1].value,
-            password: e.target[2].value
-        })
-            .then(res => {
-                setLoginFailed(false);
-                // set UserStore
-                userStore.initUserInfo(res.data.data.user);
-                // redirect
-                window.location.href = `/#${Routes.PlatformDashboard.path}`;
-            })
-            .catch(e => {
-                setLoginFailed(true);
-                throw e;
-            });
-    };
+  const onSubmitLoginForm = e => {
+    e.preventDefault();
+    LdAxios.post("/api/auth/login/", {
+      username: e.target[0].value,
+      email: e.target[1].value,
+      password: e.target[2].value
+    })
+      .then(res => {
+        setLoginFailed(false);
+        // set UserStore
+        userStore.initUserInfo(res.data.data.user);
+        // redirect
+        window.location.href = `/#${Routes.PlatformDashboard.path}`;
+      })
+      .catch(e => {
+        setLoginFailed(true);
+        throw e;
+      });
+  };
 
-    return (
-        <main>
-            <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
-                <Container>
-                    <p className="text-center">
-                        <Card.Link
-                            as={Link}
-                            to={Routes.PlatformDashboard.path}
-                            className="text-gray-700"
+  return (
+    <main>
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container>
+          <p className="text-center">
+            <Card.Link
+              as={Link}
+              to={Routes.PlatformDashboard.path}
+              className="text-gray-700"
+            >
+              <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to
+              home
+            </Card.Link>
+          </p>
+          <Row
+            className="justify-content-center form-bg-image"
+            style={{ backgroundImage: `url(${BgImage})` }}
+          >
+            <Col
+              xs={12}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                <div className="text-center text-md-center mb-4 mt-md-0">
+                  <h3 className="mb-0">Sign in to our platform</h3>
+                </div>
+                <Form className="mt-4" onSubmit={onSubmitLoginForm}>
+                  <Form.Group id="username" className="mb-4">
+                    <Form.Label>Your Username</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faIdCard} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        autoFocus
+                        required
+                        type="text"
+                        name="username"
+                        placeholder="User ID"
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group id="username" className="mb-4">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faIdCard} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        autoFocus
+                        required
+                        type="text"
+                        name="email"
+                        placeholder="User Email"
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Group id="password" className="mb-4">
+                      <Form.Label>Your Password</Form.Label>
+                      <InputGroup>
+                        <InputGroup.Text>
+                          <FontAwesomeIcon icon={faUnlockAlt} />
+                        </InputGroup.Text>
+                        <Form.Control
+                          required
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <Form.Check type="checkbox">
+                        <FormCheck.Input id="defaultCheck5" className="me-2" />
+                        <FormCheck.Label
+                          htmlFor="defaultCheck5"
+                          className="mb-0"
                         >
-                            <FontAwesomeIcon icon={faAngleLeft} className="me-2"/> Back to
-                            home
-                        </Card.Link>
-                    </p>
-                    <Row
-                        className="justify-content-center form-bg-image"
-                        style={{backgroundImage: `url(${BgImage})`}}
-                    >
-                        <Col
-                            xs={12}
-                            className="d-flex align-items-center justify-content-center"
-                        >
-                            <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
-                                <div className="text-center text-md-center mb-4 mt-md-0">
-                                    <h3 className="mb-0">Sign in to our platform</h3>
-                                </div>
-                                <Form className="mt-4" onSubmit={onSubmitLoginForm}>
-                                    <Form.Group id="username" className="mb-4">
-                                        <Form.Label>Your Username</Form.Label>
-                                        <InputGroup>
-                                            <InputGroup.Text>
-                                                <FontAwesomeIcon icon={faIdCard}/>
-                                            </InputGroup.Text>
-                                            <Form.Control
-                                                autoFocus
-                                                required
-                                                type="text"
-                                                name="username"
-                                                placeholder="User ID"
-                                            />
-                                        </InputGroup>
-                                    </Form.Group>
-                                    <Form.Group id="username" className="mb-4">
-                                        <Form.Label>Your Email</Form.Label>
-                                        <InputGroup>
-                                            <InputGroup.Text>
-                                                <FontAwesomeIcon icon={faIdCard}/>
-                                            </InputGroup.Text>
-                                            <Form.Control
-                                                autoFocus
-                                                required
-                                                type="text"
-                                                name="email"
-                                                placeholder="User Email"
-                                            />
-                                        </InputGroup>
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Group id="password" className="mb-4">
-                                            <Form.Label>Your Password</Form.Label>
-                                            <InputGroup>
-                                                <InputGroup.Text>
-                                                    <FontAwesomeIcon icon={faUnlockAlt}/>
-                                                </InputGroup.Text>
-                                                <Form.Control
-                                                    required
-                                                    type="password"
-                                                    name="password"
-                                                    placeholder="Password"
-                                                />
-                                            </InputGroup>
-                                        </Form.Group>
-                                        <div className="d-flex justify-content-between align-items-center mb-4">
-                                            <Form.Check type="checkbox">
-                                                <FormCheck.Input id="defaultCheck5" className="me-2"/>
-                                                <FormCheck.Label
-                                                    htmlFor="defaultCheck5"
-                                                    className="mb-0"
-                                                >
-                                                    Remember me
-                                                </FormCheck.Label>
-                                            </Form.Check>
-                                            <Card.Link className="small text-end">
-                                                Lost password?
-                                            </Card.Link>
-                                        </div>
-                                    </Form.Group>
-                                    <Button variant="primary" type="submit" className="w-100">
-                                        Log in
-                                    </Button>
-                                    {loginFailed && (
-                                        <Form.Text className="text-danger">Login failed</Form.Text>
-                                    )}
-                                </Form>
+                          Remember me
+                        </FormCheck.Label>
+                      </Form.Check>
+                      <Card.Link className="small text-end">
+                        Lost password?
+                      </Card.Link>
+                    </div>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100">
+                    Log in
+                  </Button>
+                  {loginFailed && (
+                    <Form.Text className="text-danger">Login failed</Form.Text>
+                  )}
+                </Form>
 
-                                <div className="d-flex justify-content-center align-items-center mt-4">
+                <div className="d-flex justify-content-center align-items-center mt-4">
                   <span className="fw-normal">
                     Not registered?
                     <Card.Link
-                        as={Link}
-                        to={Routes.Signup.path}
-                        className="fw-bold"
+                      as={Link}
+                      to={Routes.Signup.path}
+                      className="fw-bold"
                     >
                       {` Sign up `}
                     </Card.Link>
                   </span>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </main>
-    );
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
 };
