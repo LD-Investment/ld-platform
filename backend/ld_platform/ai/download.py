ab461b5479c6e43149004884e00971f14fbdd062
Indosaram <freedom_zero@naver.com>
2022-05-22 22:26:30 +0900

    Download pretrained models at build time

--- backend/ld_platform/ai/download.py
+++ backend/ld_platform/ai/download.py
@@ -3,29 +3,24 @@ import logging
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    logger.info("Checking if models exist...")
+BASE_MODEL_PATH = "/models"
+# FIXME: AUTH_TOKEN should be read from .envs/.local/.ai
+AUTH_TOKEN = "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"
+CRYPTODEBERTA_MODEL_NAME = "CryptoModel/cryptodeberta-base-all-finetuned"
 
-    base_model_path = os.environ["BASE_MODEL_PATH"]
-    model_name = os.environ["HUGGING_FACE_CRYPTO_DEBERTA_MODEL_NAME"]
-    auth_token = os.environ["HUGGING_FACE_CRYPTO_DEBERTA_AUTH_TOKEN"]
+model_save_path = os.path.join(BASE_MODEL_PATH, CRYPTODEBERTA_MODEL_NAME)
 
-    model_save_path = os.path.join(base_model_path, model_name)
+# TODO: Automatically download the new models
+if not os.path.exists(os.path.join(model_save_path, "tokenizer_config.json")):
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=CRYPTODEBERTA_MODEL_NAME, use_auth_token=AUTH_TOKEN
+    )
+    tokenizer.save_pretrained(model_save_path)
 
-    # TODO: Automatically download the new models
-    if not os.path.exists(os.path.join(model_save_path, "tokenizer_config.json")):
-        logger.info("Starting downloading a tokenizer..." )
-        tokenizer = AutoTokenizer.from_pretrained(
-            pretrained_model_name_or_path=model_name, use_auth_token=auth_token
-        )
-        tokenizer.save_pretrained(model_save_path)
-
-    if not os.path.exists(os.path.join(model_save_path, "config.json")):
-        logger.info("Starting downloading a model..." )
-        model = AutoModelForSequenceClassification.from_pretrained(
-            pretrained_model_name_or_path=model_name,
-            use_auth_token=auth_token,
-            num_labels=3,
-        )
-        model.save_pretrained(model_save_path)
+if not os.path.exists(os.path.join(model_save_path, "config.json")):
+    model = AutoModelForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path=CRYPTODEBERTA_MODEL_NAME,
+        use_auth_token=AUTH_TOKEN,
+        num_labels=3,
+    )
+    model.save_pretrained(model_save_path)
